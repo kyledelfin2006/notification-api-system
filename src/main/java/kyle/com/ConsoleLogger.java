872aba397da2dc38fb7b4b5b69f525e0c131713f
kyledelfin2006 <aldrinkyles@1219@gmail.com>
2026-03-08 03:21:46 +0800

    Initial commit: Notification Dispatch System

---
+++ src/main/java/kyle/com/ConsoleLogger.java
@@ -0,0 +1,23 @@
+package kyle.com;
+public class ConsoleLogger implements Logger {
+
+    @Override
+    public void info(String message) {
+        System.out.println("[INFO] " + message);
+    }
+
+    @Override
+    public void error(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+
+    @Override
+    public void debug(String message) {
+        System.out.println("[DEBUG] " + message);
+    }
+
+    @Override
+    public void warn(String message) {
+        System.out.println("[WARNING] " + message);
+    }
+}
---
+++ src/main/java/kyle/com/Storage.java
@@ -0,0 +1,9 @@
+package kyle.com;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Storage<T> {
+    void save(List<T> items) throws IOException;
+    List<T> load() throws IOException;
+}
