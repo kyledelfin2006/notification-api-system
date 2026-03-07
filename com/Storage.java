024d17af96a6d5949ded135aaf95bd49b7877c1c
Elyk <aldrinkyles1219@gmail.com>
2026-03-08 03:25:10 +0800

    Delete com directory

--- com/Storage.java
+++
@@ -1,9 +0,0 @@
-package kyle.com;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface Storage<T> {
-    void save(List<T> items) throws IOException;
-    List<T> load() throws IOException;
-}
